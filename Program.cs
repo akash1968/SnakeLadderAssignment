1048f89c006917bc661bc816805e5e3b566af606
Akash <akash.singh670@gmail.com>
2020-10-06 19:03:25 +0530

    [Akash] .Added UC1 of Snake Ladder

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadderAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerPosition = 0;
+            Console.WriteLine("Snake and Ladder game played with single player at position " + playerPosition);
+        }
+    }
+}
